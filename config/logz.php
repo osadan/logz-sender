a07dcf1aced4fc65e52b6c572c003d3a92ee783c
Ohad Sadan <ohad.s@compie.co.il>
2023-04-03 18:29:19 +0300

    modify the code

--- config/logz.php
+++ config/logz.php
@@ -1,12 +1,11 @@
 <?php
-  return [
-    'logzioToken' => env("LOGZIO_TOKEN", null),
-    'logzioListener' => env("LOGZIO_LISTENER_ADDRESS", 'listener.logz.io'),
-    'logzioRegion' => env("LOGZIO_REGION", 'us'),
-    'logzioAccount' => env("LOGZIO_ACCOUNT" , null),
-    'logzioEndPoint' => env("LOGZIO_ENDPOINT", null),
-    'disabledPathParams' => env("PATH_PARAMS_DISABLED", ['password','login']),
-    'disabledUris' => env("DISABLED_URIS", ['favicon.ico']),
-    'queryCaptureMinTime' => env("LOGZIO_QUERY_CAPTURE_MIN_TIME", 0.250)
-  ];
-  
+return [
+  'logzioToken' => env("LOGZIO_TOKEN", null),
+  'logzioListener' => env("LOGZIO_LISTENER_ADDRESS", 'listener.logz.io'),
+  'logzioRegion' => env("LOGZIO_REGION", 'us'),
+  'logzioAccount' => env("LOGZIO_ACCOUNT", null),
+  'logzioEndPoint' => env("LOGZIO_ENDPOINT", null),
+  'disabledPathParams' => explode(',', env("PATH_PARAMS_DISABLED", 'password,login')),
+  'disabledUris' => explode(',', env("DISABLED_URIS", 'favicon.ico,/')),
+  'queryCaptureMinTime' => env("LOGZIO_QUERY_CAPTURE_MIN_TIME", 0.250)
+];
